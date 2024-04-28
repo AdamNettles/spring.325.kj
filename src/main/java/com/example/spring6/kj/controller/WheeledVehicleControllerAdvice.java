2182e136e80b293c46dce068f8a7bdbd6b2779d9
Adam Nettles <adam_r_nettles@proton.me>
2024-04-28 12:01:34 -0500

    Log text coloring

--- src/main/java/com/example/spring6/kj/controller/WheeledVehicleControllerAdvice.java
+++ src/main/java/com/example/spring6/kj/controller/WheeledVehicleControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.example.spring6.kj.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @ControllerAdvice(annotations = RestController.class)
 public class WheeledVehicleControllerAdvice {
 
+    private static final Logger log = LoggerFactory.getLogger(WheeledVehicleControllerAdvice.class);
+
     @ExceptionHandler({ControllerException.class})
     public final ResponseEntity<ApiError> handleException(Exception e) {
+        log.error(e.getMessage());
         if(e instanceof ControllerException) {
             return ResponseEntity.badRequest().body(new ApiError(e.getMessage()));
         } else {
